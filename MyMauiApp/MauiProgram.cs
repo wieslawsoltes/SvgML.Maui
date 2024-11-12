10fd5045569b9e2ed0fa7830aed6622aca73dece
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-11-12 23:46:25 +0100

    Initial commit

---
+++ MyMauiApp/MauiProgram.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+using SvgML;
+
+namespace MyMauiApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		
+		// TODO: Add UseSvgML() extension method to MauiAppBuilder
+		GC.KeepAlive(typeof(svg));
+        GC.KeepAlive(typeof(defs));
+        GC.KeepAlive(typeof(linearGradient));
+        GC.KeepAlive(typeof(stop));
+        GC.KeepAlive(typeof(rect));
+        GC.KeepAlive(typeof(circle));
+
+		
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			// TODO:
+			.UseSkiaSharp()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
---
+++ SvgML.Maui/Maui/Document Structure/svg.Control.cs
@@ -0,0 +1,240 @@
+using SkiaSharp;
+using SkiaSharp.Views.Maui;
+using Svg.Model;
+using Svg.Skia;
+
+namespace SvgML;
+
+/// <summary>
+/// Svg control.
+/// </summary>
+public partial class svg
+{
+    protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+    {
+        base.OnPaintSurface(e);
+        
+        var canvas = e.Surface.Canvas;
+        
+        // TODO:
+        // canvas.DrawColor(SKColors.Aqua);
+
+        var source = this;
+        
+        var css = GetCss(this);
+        var currentCss = GetCurrentCss(this);
+        var parameters = new SvgParameters(null, string.Concat(css, ' ', currentCss));
+        Load(source, parameters);
+
+        var _svg = this;
+        
+        lock (_svg.Sync)
+        {
+            var picture = _svg.Picture;
+            if (picture is null)
+            {
+                return;
+            }
+
+            canvas.Save();
+            canvas.DrawPicture(picture);
+            canvas.Restore();
+        }
+
+    }
+
+    static svg()
+    {
+        // TODO:
+        // ClipToBoundsProperty.OverrideDefaultValue(typeof(svg), true);
+
+        s_skiaModel = new SkiaModel(new SKSvgSettings());
+        s_assetLoader = new SkiaAssetLoader(s_skiaModel);
+
+        // TODO:
+        // AffectsRender<svg>(StretchProperty, StretchDirectionProperty);
+        // AffectsMeasure<svg>(StretchProperty, StretchDirectionProperty);
+
+        // TODO:
+        // CssProperty.Changed.AddClassHandler<Control>(OnCssPropertyAttachedPropertyChanged);
+        // CurrentCssProperty.Changed.AddClassHandler<Control>(OnCssPropertyAttachedPropertyChanged);
+    }
+
+    // TODO:
+    /*
+    private static void OnCssPropertyAttachedPropertyChanged(AvaloniaObject d, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (d is Control control)
+        {
+            control.InvalidateMeasure();
+            control.InvalidateVisual();
+        }
+    }
+    */
+
+    // TODO:
+    /*
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        if (_picture == null)
+        {
+            return new Size();
+        }
+
+        var sourceSize = _picture is { }
+            ? new Size(_picture.CullRect.Width, _picture.CullRect.Height)
+            : default;
+
+        return Stretch.CalculateSize(availableSize, sourceSize, StretchDirection);
+    }
+    */
+    protected override Size MeasureOverride(double widthConstraint, double heightConstraint)
+    {
+        return base.MeasureOverride(widthConstraint, heightConstraint);
+    }
+
+    // TODO:
+    /*
+    protected override Size ArrangeOverride(Size finalSize)
+    {
+        if (_picture == null)
+        {
+            return new Size();
+        }
+
+        var sourceSize = _picture is { }
+            ? new Size(_picture.CullRect.Width, _picture.CullRect.Height)
+            : default;
+
+        return Stretch.CalculateSize(finalSize, sourceSize);
+    }
+    */
+    protected override Size ArrangeOverride(Rect bounds)
+    {
+        return base.ArrangeOverride(bounds);
+    }
+
+    /*
+    public override void Render(DrawingContext context)
+    {
+        var source = _picture;
+        if (source is null)
+        {
+            return;
+        }
+
+        var viewPort = new Rect(Bounds.Size);
+        var sourceSize = new Size(source.CullRect.Width, source.CullRect.Height);
+        if (sourceSize.Width <= 0 || sourceSize.Height <= 0)
+        {
+            return;
+        }
+
+        var scale = Stretch.CalculateScaling(Bounds.Size, sourceSize, StretchDirection);
+        var scaledSize = sourceSize * scale;
+        var destRect = viewPort
+            .CenterRect(new Rect(scaledSize))
+            .Intersect(viewPort);
+        var sourceRect = new Rect(sourceSize)
+            .CenterRect(new Rect(destRect.Size / scale));
+
+        var bounds = source.CullRect;
+        var scaleMatrix = Matrix.CreateScale(
+            destRect.Width / sourceRect.Width,
+            destRect.Height / sourceRect.Height);
+        var translateMatrix = Matrix.CreateTranslation(
+            -sourceRect.X + destRect.X - bounds.Top,
+            -sourceRect.Y + destRect.Y - bounds.Left);
+
+        using var _ = ClipToBounds ? context.PushClip(destRect) : default;
+
+        using (context.PushTransform(translateMatrix * scaleMatrix))
+        {
+            context.Custom(
+                new SKPictureCustomDrawOperation(
+                    new Rect(0, 0, bounds.Width, bounds.Height),
+                    this));
+        }
+    }
+    */
+
+    protected override void Invalidate()
+    {
+        base.Invalidate();
+
+        // TODO: Only invalidate SvgSource if its Svg property that changed.
+
+        if (IsLoaded)
+        {
+            // TODO:
+            // OnSourceChanged(this);
+        }
+    }
+
+    // TODO:
+    /*
+    /// <inheritdoc/>
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == CssProperty)
+        {
+            var css = change.GetNewValue<string?>();
+            OnCssChanged(css);
+        }
+
+        if (change.Property == CurrentCssProperty)
+        {
+            var currentCss = change.GetNewValue<string?>();
+            OnCurrentCssChanged(currentCss);
+        }
+
+        if (change.Property == ClipToBoundsProperty)
+        {
+            InvalidateVisual();
+        }
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+
+        OnSourceChanged(this);
+    }
+    */
+
+    // TODO:
+    /*
+    private void OnCssChanged(string? css)
+    {
+        var source = this;
+        var currentCss = GetCurrentCss(this);
+        var parameters = new SvgParameters(null, string.Concat(css, ' ', currentCss));
+        Load(source, parameters);
+        InvalidateMeasure();
+        InvalidateVisual();
+    }
+
+    private void OnCurrentCssChanged(string? currentCss)
+    {
+        var source = this;
+        var css = GetCss(this);
+        var parameters = new SvgParameters(null, string.Concat(css, ' ', currentCss));
+        Load(source, parameters);
+        InvalidateMeasure();
+        InvalidateVisual();
+    }
+
+    private void OnSourceChanged(svg? source)
+    {
+        var css = GetCss(this);
+        var currentCss = GetCurrentCss(this);
+        var parameters = new SvgParameters(null, string.Concat(css, ' ', currentCss));
+        Load(source, parameters);
+        InvalidateMeasure();
+        InvalidateVisual();
+    }
+    */
+
+}
