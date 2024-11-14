b124c3ec1d67156e46d23c3fa038f6240a6327ca
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-11-14 21:44:27 +0100

    Update MauiProgram.cs

--- MyMauiApp/MauiProgram.cs
+++ MyMauiApp/MauiProgram.cs
@@ -11,23 +11,14 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
-		
-		// TODO: Add UseSvgML() extension method to MauiAppBuilder
-        // TODO: Add all types from SvgML.Maui into UseSvgML() extension method
-		GC.KeepAlive(typeof(svg));
-        GC.KeepAlive(typeof(defs));
-        GC.KeepAlive(typeof(linearGradient));
-        GC.KeepAlive(typeof(stop));
-        GC.KeepAlive(typeof(rect));
-        GC.KeepAlive(typeof(circle));
-
-        GC.KeepAlive(typeof(Canvas));
-		
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+ 
 			// TODO:
 			.UseSkiaSharp()
+            .UseSvgML()
+
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
